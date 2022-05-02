1f5a6c75861c9486d83bc44530f26c7cf3c8ecb8
nohhow <xksrma97@gmail.com>
2022-05-02 23:56:50 +0900

    [✨Feat] : 제품 상세 페이지 추가

---
+++ client/src/pages/InfoPage/itemInfo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ItemInfo() {
+  return (
+    <div>
+        <section id="itemInfo_section">
+            제품상세 페이지
+        </section>
+    </div>
+  )
+}
+
+export default ItemInfo
