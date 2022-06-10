aa812a13c8e60bc1fea8c1e557fb6e4efbe18692
nohhow <xksrma97@gmail.com>
2022-06-11 00:53:52 +0900

    [🔨Refactor] : > 기호 사용한 것이 eslint에 걸려서 수정

--- client/src/pages/CartPage/OrderComplete.js
+++ client/src/pages/CartPage/OrderComplete.js
@@ -5,8 +5,7 @@ function OrderComplete() {
     <main id="order-complete-section">
       <h1>주문 성공!</h1>
       <hr/>
-      <p>주문 현황은 <mark>내 정보 > 주문현황</mark> 에서 확인하실 수 있으며, 배송 날짜가 확정되면 가입할 때 <u>작성한 이메일</u>로 전달됩니다.</p>
-      
+      <p>주문 현황은 <mark>내 정보 `{'>'}` 주문현황</mark> 에서 확인하실 수 있으며, 배송 날짜가 확정되면 가입할 때 <u>작성한 이메일</u>로 전달됩니다.</p>
     </main>
   )
 }
