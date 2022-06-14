7ccda1527940fadd3b85107a754b37687acaf3a8
nohhow <xksrma97@gmail.com>
2022-06-14 20:43:10 +0900

    [💄style] : 주문완료 페이지에서 confetti(색종이조각)효과 추가

---
+++ client/src/hooks/useWindowDimensions.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+export default function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+    console.log("꽃가루를 날려~~")
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
